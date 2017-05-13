14c3327347a7980432fc1babae0e257e0da45aee
lalitkumarsonawane <lalitsonawane@gmail.com>
2017-05-13 22:26:25 +0530

    Comment added 004

--- app/src/main/java/in/apptonic/lalit/cdlabsconnect/MainActivity.java
+++ app/src/main/java/in/apptonic/lalit/cdlabsconnect/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //004
+        //005
     }
 }
